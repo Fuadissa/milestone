4f03547eb9ae2dc2b02722727901dc8fd296e2a5
Fuad <issafuad1818@gmail.com>
2025-01-30 07:50:01 -0800

    Add Home component to render Mint and Successful components

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Mint } from "@/components/Mint";
+import { Navbar } from "@/components/Navbar";
+import { Successful } from "@/components/Successful";
+
+export default function Home() {
+  return (
+    <div className="">
+
+      <Mint />
+
+      <Successful />
+    </div>
+  );
+}
